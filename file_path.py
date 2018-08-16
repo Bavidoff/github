e4604b6ad5911f6b0dc1b1e5e09e2ce6b1ff4257
Ba Linh <vubalinh@hotmail.com>
2018-08-16 17:28:25 +0200

    first commit

---
+++ file_path.py
@@ -0,0 +1,19 @@
+def CtoD():
+    for node in nuke.selectedNodes():
+        if node.Class() == 'Read':
+            nsplit = node['file'].value().split('/') 
+            if nsplit[0] == 'E:':
+                nsplit[0] = 'D:'  
+            elif nsplit[0] == 'D:':
+                nsplit[0] = 'E:'
+            njoin = '/'.join(nsplit)
+            node['file'].setValue(njoin)
+CtoD()
+
+
+
+p = nuke.Panel('switch drive')
+p.addButton('change to C:')
+p.addButton('change to D:')
+
+p.show()
